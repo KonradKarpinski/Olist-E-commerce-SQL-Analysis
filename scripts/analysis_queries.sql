fd18e76071adf5b498dcd799eca3b04ad46c3cae
Konrad Karpiński <karpinski.konrad13@gmail.com>
2026-08-23 23:16:04 +0200

    Refactor SQL queries for clarity and consistency

--- scripts/analysis_queries.sql
+++ scripts/analysis_queries.sql
@@ -1,6 +1,6 @@
-/* ==========================================================
-TASK 1: Monthly Revenue
-==========================================================
+/* ====================================================
+Task 1: Revenue & Sales Trend / Step A: Monthly Revenue
+=======================================================
 */
 
 SELECT 
@@ -13,28 +13,28 @@ WHERE orders.order_status = 'delivered'
 GROUP BY time 
 ORDER BY time ASC;
 
-/* ==========================================================
--- Task 2: Top Product Categories
-==========================================================
+/* =======================================================================
+Task 2: Product portfolio & Profitability / Step A: Top Product Categories
+==========================================================================
 */
 
-SELECT trans.product_category_name_english AS category, ROUND(SUM(items.price + items.freight_value),2) AS sale,
-COUNT (items.product_id) AS number_of_items_sold, AVG(items.price) AS avg_price
+SELECT 
+COALESCE(trans.product_category_name_english, products.product_category_name) AS category, 
+ROUND(SUM(items.price + items.freight_value),2) AS sale,
+COUNT (items.product_id) AS number_of_items_sold, 
+ROUND(AVG(items.price), 2) AS avg_price
 FROM olist_orders_dataset AS orders
-INNER JOIN olist_order_items_dataset AS items
-ON orders.order_id = items.order_id 
-INNER JOIN olist_products_dataset AS products
-ON items.product_id = products.product_id
-INNER JOIN product_category_name_translation AS trans
-ON products.product_category_name = trans.product_category_name 
+INNER JOIN olist_order_items_dataset AS items ON orders.order_id = items.order_id 
+INNER JOIN olist_products_dataset AS products ON items.product_id = products.product_id
+LEFT JOIN product_category_name_translation AS trans ON products.product_category_name = trans.product_category_name 
 WHERE orders.order_status = 'delivered'
-GROUP BY trans.product_category_name_english
+GROUP BY category
 ORDER BY sale DESC 
-LIMIT (10);
+LIMIT 10;
 
-/* ==========================================================
--- Task 3: Regional Delivery Delays
-==========================================================
+/* ========================================================================
+Task 3: Logistics & Delivery Performance / Step A: Regional Delivery Delays
+===========================================================================
 */
 
 WITH delivery_performance AS (
@@ -47,7 +47,6 @@ END AS delivery_status
 FROM olist_orders_dataset AS orders
 JOIN olist_customers_dataset AS customers ON orders.customer_id = customers.customer_id
 WHERE orders.order_status = 'delivered'
-AND orders.order_delivered_customer_date IS NOT NULL
 )
 SELECT customer_state,
 COUNT(*) AS total_orders,
@@ -58,9 +57,33 @@ GROUP BY customer_state
 ORDER BY percentage_very_late DESC
 LIMIT 5;
 
-/* ==========================================================
--- Task 4: Customer Retention
-==========================================================
+/* ==================================================================
+Task 3: Logistics & Delivery Performance / Step B: The Cost Of Delays
+=====================================================================
+*/
+
+WITH delivery_and_reviews AS (
+SELECT orders.order_id, reviews.review_score,
+CASE 
+WHEN (julianday(orders.order_delivered_customer_date) - julianday(orders.order_estimated_delivery_date)) > 3 THEN 'Very Late'
+WHEN (julianday(orders.order_delivered_customer_date) - julianday(orders.order_estimated_delivery_date)) > 0 THEN 'Slightly Late'
+ELSE 'On-time or Early'
+END AS delivery_status
+FROM olist_orders_dataset AS orders
+JOIN olist_order_reviews_dataset AS reviews ON orders.order_id = reviews.order_id
+WHERE orders.order_status = 'delivered'
+)
+SELECT 
+delivery_status,
+COUNT(*) AS number_of_reviews,
+ROUND(AVG(review_score), 2) AS average_review_score
+FROM delivery_and_reviews
+GROUP BY delivery_status
+ORDER BY average_review_score DESC;
+
+/* ==============================================================================
+Task 4: Customer Retention & Lifecycle / Step A: One-Time vs. Returning Customers
+=================================================================================
 */
 
 WITH customer_order_counts AS (
@@ -88,28 +111,3 @@ ROUND(AVG(total_spent / order_count), 2) AS avg_order_value,
 ROUND(COUNT(customer_unique_id) * 100.0 / (SELECT COUNT(*) FROM customer_segmentation), 2) AS percentage_share
 FROM customer_segmentation
 GROUP BY loyalty_segment;
-
-/* ==========================================================
--- Task 5: Delivery vs Satisfaction
-==========================================================
-*/
-
-WITH delivery_and_reviews AS (
-SELECT orders.order_id, reviews.review_score,
-CASE 
-WHEN (julianday(orders.order_delivered_customer_date) - julianday(orders.order_estimated_delivery_date)) > 3 THEN 'Very Late'
-WHEN (julianday(orders.order_delivered_customer_date) - julianday(orders.order_estimated_delivery_date)) > 0 THEN 'Slightly Late'
-ELSE 'On-time or Early'
-END AS delivery_status
-FROM olist_orders_dataset AS orders
-JOIN olist_order_reviews_dataset AS reviews ON orders.order_id = reviews.order_id
-WHERE orders.order_status = 'delivered'
-AND orders.order_delivered_customer_date IS NOT NULL
-)
-SELECT 
-delivery_status,
-COUNT(*) AS number_of_reviews,
-ROUND(AVG(review_score), 2) AS average_review_score
-FROM delivery_and_reviews
-GROUP BY delivery_status
-ORDER BY average_review_score DESC;
